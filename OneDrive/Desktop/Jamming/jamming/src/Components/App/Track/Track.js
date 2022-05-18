a4e8cf70aa6135acce8bcc772b4e747cbb39eaa6
Sanjog Wanem <sjwanem@gmail.com>
2022-05-18 12:56:36 +0100

    18/05/2022 Section 3 done

--- OneDrive/Desktop/Jamming/jamming/src/Components/App/Track/Track.js
+++ OneDrive/Desktop/Jamming/jamming/src/Components/App/Track/Track.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import './Track.css';
 
 class Track extends React.Component {
+
+    renderAction() {
+        
+        if (this.props.isRemoval)
+        {
+            return <button className="Track-Action">-</button>
+        }
+        else 
+        {
+            return <button className="Track-Action">+</button>
+        }
+    }
+
     render() {
         return(
-            <div class="Track">
-                <div class="Track-information">
-                    <h3><!-- track name will go here --></h3>
-                    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+            <div className="Track">
+                <div className="Track-information">
+                    <h3>{this.props.track.name}</h3>
+                    <p> {this.props.track.artist} | {this.props.track.album} </p>
                 </div>
-                <button class="Track-action"><!-- + or - will go here --></button>
+                {this.renderAction()};
             </div>
         )
     }
 }
+
+export default Track;
